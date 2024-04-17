29bc010198b980bd1d7174f5f8df7ba9bf9f35fb
seyoungree <seyoungree@gmail.com>
2024-04-17 16:44:47 -0400

    added approach for quantizing ortho matrices

--- galore_torch/galore_projector.py
+++ galore_torch/galore_projector.py
@@ -1,4 +1,5 @@
 import torch
+import bitsandbytes.functional
 
 class GaLoreProjector:
     def __init__(self, rank, verbose=False, update_proj_gap=200, scale=1.0, proj_type='std'):
@@ -40,15 +41,25 @@ class GaLoreProjector:
         elif self.proj_type == 'full':
             if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
                 self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='full')
-            low_rank_grad = torch.matmul(self.ortho_matrix[0].t(), full_rank_grad) @ self.ortho_matrix[1].t()
-                
+                low_rank_grad = torch.matmul(self.ortho_matrix[0].t(), full_rank_grad) @ self.ortho_matrix[1].t()
+                # quantize orthomatrices
+                # bitsandbytes.functional.quantize(self.ortho_matrix)
+            else:
+                # dequantize orthomatrices
+                # bitsandbytes.functional.dequantize(self.ortho_matrix)
+                low_rank_grad = torch.matmul(self.ortho_matrix[0].t(), full_rank_grad) @ self.ortho_matrix[1].t()
+                # quanitze orthomatrices
+                # bitsandbytes.functional.quantize(self.ortho_matrix)
+
         return low_rank_grad
 
     def project_back(self, low_rank_grad):
 
         if self.proj_type == 'std':
             if low_rank_grad.shape[0] >= low_rank_grad.shape[1]:
+                # dequantize orthomatrices
                 full_rank_grad = torch.matmul(low_rank_grad, self.ortho_matrix)
+                # quantize orthomatrices
             else:
                 full_rank_grad = torch.matmul(self.ortho_matrix, low_rank_grad)
         elif self.proj_type == 'reverse_std':
