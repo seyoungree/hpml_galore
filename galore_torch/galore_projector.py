5cd6fcb183008b06160c258ee8144c2592ff177f
albjan <aj3051@columbia.edu>
2024-04-24 08:03:11 +0000

    implementedbasic quantization

--- galore_torch/galore_projector.py
+++ galore_torch/galore_projector.py
@@ -1,5 +1,12 @@
 import torch
-import bitsandbytes.functional
+import bitsandbytes.functional as bnbf
+
+# Ideally, we make these command line arguments instead. However, since we are using LLama-Factory to benchmark,
+# we'd have to also modify the Llama-Factory package to accomodate for the new argument. For now, we can just hardcode these flags
+# and modify it in between runs, so we don't have to touch Llama-Factory.  
+
+QUANTIZE = None # if quantizing, choose one of 'fp8', 'fp4', 'fp8_blockwise' - otherwise, set to None
+BLOCKSZ = 128 # only matters if QUANTIZE = 'fp8_blockwise'
 
 class GaLoreProjector:
     def __init__(self, rank, verbose=False, update_proj_gap=200, scale=1.0, proj_type='std'):
@@ -9,18 +16,30 @@ class GaLoreProjector:
         self.scale = scale
         self.ortho_matrix = None
         self.proj_type = proj_type
-        
+
+    # only added quantization functionality for 'std'
     def project(self, full_rank_grad, iter):
-        
-        if self.proj_type == 'std':
+        if self.proj_type == 'std': 
             if full_rank_grad.shape[0] >= full_rank_grad.shape[1]:
                 if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
                     self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='right')
+                    if QUANTIZE:
+                        self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ)  
+                if QUANTIZE:
+                    self.ortho_matrix = self._dequantize(self.ortho_matrix, QUANTIZE, self.quantization_state, full_rank_grad.dtype) # dequantize
                 low_rank_grad = torch.matmul(full_rank_grad, self.ortho_matrix.t())
+                if QUANTIZE:
+                    self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ) # quantize back for storage
             else:
                 if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
                     self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='left')
+                    if QUANTIZE:
+                        self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ)  
+                if QUANTIZE:
+                    self.ortho_matrix = self._dequantize(self.ortho_matrix, QUANTIZE, self.quantization_state, full_rank_grad.dtype) # dequantize         
                 low_rank_grad = torch.matmul(self.ortho_matrix.t(), full_rank_grad)
+                if QUANTIZE:
+                    self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE) # quantize back for storage
         elif self.proj_type == 'reverse_std':
             if full_rank_grad.shape[0] >= full_rank_grad.shape[1]:
                 if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
@@ -56,12 +75,14 @@ class GaLoreProjector:
     def project_back(self, low_rank_grad):
 
         if self.proj_type == 'std':
+            if QUANTIZE:
+                self.ortho_matrix = self._dequantize(self.ortho_matrix, QUANTIZE, self.quantization_state, low_rank_grad.dtype) # dequantize
             if low_rank_grad.shape[0] >= low_rank_grad.shape[1]:
-                # dequantize orthomatrices
                 full_rank_grad = torch.matmul(low_rank_grad, self.ortho_matrix)
-                # quantize orthomatrices
             else:
                 full_rank_grad = torch.matmul(self.ortho_matrix, low_rank_grad)
+            if QUANTIZE:
+                self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ) # quantize back for storage
         elif self.proj_type == 'reverse_std':
             if low_rank_grad.shape[0] <= low_rank_grad.shape[1]: # note this is different from std
                 full_rank_grad = torch.matmul(self.ortho_matrix, low_rank_grad)
@@ -97,9 +118,8 @@ class GaLoreProjector:
         if type=='right':
             A = U[:, :rank] @ torch.diag(s[:rank])
             B = Vh[:rank, :]
-            
             if not float_data:
-                B = B.to(original_device).type(original_type)
+                B = B.to(original_device).type(original_type) 
             return B
         elif type=='left':
             A = U[:, :rank]
@@ -117,5 +137,24 @@ class GaLoreProjector:
         else:
             raise ValueError('type should be left, right or full')
 
+    def _quantize(self, A, quantize_type, bsz=4096):
+        # print("QUANTIZING")
+        if quantize_type == 'fp8':
+            return bnbf.quantize(A)
+        elif quantize_type == 'fp4':
+            return bnbf.quantize_4bit(A)
+        elif quantize_type == 'fp8_blockwise':
+            return bnbf.quantize_blockwise(A, blocksize=bsz)
+        else:
+            raise ValueError("Invalid quantization type")
 
-
+    def _dequantize(self, A, quantize_type, state, dtype):
+        # print("DEQUANTIZING")
+        if quantize_type == 'fp8':
+            return bnbf.dequantize(A, state).to(dtype) # dequantizes to fp32 by default without .to(dtype)
+        elif quantize_type == 'fp4':
+            return bnbf.dequantize_4bit(A, state).to(dtype)
+        elif quantize_type == 'fp8_blockwise':
+            return bnbf.dequantize_blockwise(A, state).to(dtype)
+        else:
+            raise ValueError("Invalid quantization type")
