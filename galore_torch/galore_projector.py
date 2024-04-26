90b3ddeb53d782b944727ceb9f83f99476845066
Seyoung Ree <seyoungree@gmail.com>
2024-04-26 19:47:15 +0000

    cleaning up and added implementation for all except for full

--- galore_torch/galore_projector.py
+++ galore_torch/galore_projector.py
@@ -23,75 +23,76 @@ class GaLoreProjector:
             if full_rank_grad.shape[0] >= full_rank_grad.shape[1]:
                 if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
                     self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='right')
-                    if QUANTIZE:
-                        self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ)  
-                if QUANTIZE:
-                    self.ortho_matrix = self._dequantize(self.ortho_matrix, QUANTIZE, self.quantization_state, full_rank_grad.dtype) # dequantize
+                    if QUANTIZE: self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ)  
+                if QUANTIZE: self.ortho_matrix = self._dequantize(self.ortho_matrix, QUANTIZE, self.quantization_state, full_rank_grad.dtype) # dequantize
                 low_rank_grad = torch.matmul(full_rank_grad, self.ortho_matrix.t())
-                if QUANTIZE:
-                    self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ) # quantize back for storage
             else:
                 if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
                     self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='left')
-                    if QUANTIZE:
-                        self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ)  
-                if QUANTIZE:
-                    self.ortho_matrix = self._dequantize(self.ortho_matrix, QUANTIZE, self.quantization_state, full_rank_grad.dtype) # dequantize         
+                    if QUANTIZE: self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ)  
+                if QUANTIZE: self.ortho_matrix = self._dequantize(self.ortho_matrix, QUANTIZE, self.quantization_state, full_rank_grad.dtype) # dequantize         
                 low_rank_grad = torch.matmul(self.ortho_matrix.t(), full_rank_grad)
-                if QUANTIZE:
-                    self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE) # quantize back for storage
+                
         elif self.proj_type == 'reverse_std':
             if full_rank_grad.shape[0] >= full_rank_grad.shape[1]:
                 if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
                     self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='left')
+                    if QUANTIZE: self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ)
+                if QUANTIZE: self.ortho_matrix = self._dequantize(self.ortho_matrix, QUANTIZE, self.quantization_state, full_rank_grad.dtype)
                 low_rank_grad = torch.matmul(self.ortho_matrix.t(),full_rank_grad)
             else:
                 if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
                     self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='right')
+                    if QUANTIZE: self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ)
+                if QUANTIZE: self.ortho_matrix = self._dequantize(self.ortho_matrix, QUANTIZE, self.quantization_state, full_rank_grad.dtype)
                 low_rank_grad = torch.matmul(full_rank_grad,self.ortho_matrix.t())
+
         elif self.proj_type == 'right':
             if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
                 self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='right')
+                if QUANTIZE: self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ)
+            if QUANTIZE: self.ortho_matrix = self._dequantize(self.ortho_matrix, QUANTIZE, self.quantization_state, full_rank_grad.dtype)
             low_rank_grad = torch.matmul(full_rank_grad, self.ortho_matrix.t())
+
         elif self.proj_type == 'left':
             if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
                 self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='left')
+                if QUANTIZE: self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ)
+            if QUANTIZE: self.ortho_matrix = self._dequantize(self.ortho_matrix, QUANTIZE, self.quantization_state, full_rank_grad.dtype)
             low_rank_grad = torch.matmul(self.ortho_matrix.t(), full_rank_grad)
+
         elif self.proj_type == 'full':
             if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
                 self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='full')
-                low_rank_grad = torch.matmul(self.ortho_matrix[0].t(), full_rank_grad) @ self.ortho_matrix[1].t()
-                # quantize orthomatrices
-                # bitsandbytes.functional.quantize(self.ortho_matrix)
-            else:
-                # dequantize orthomatrices
-                # bitsandbytes.functional.dequantize(self.ortho_matrix)
-                low_rank_grad = torch.matmul(self.ortho_matrix[0].t(), full_rank_grad) @ self.ortho_matrix[1].t()
-                # quanitze orthomatrices
-                # bitsandbytes.functional.quantize(self.ortho_matrix)
-
+            low_rank_grad = torch.matmul(self.ortho_matrix[0].t(), full_rank_grad) @ self.ortho_matrix[1].t()
+        
+        if QUANTIZE: self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ)
         return low_rank_grad
 
     def project_back(self, low_rank_grad):
 
         if self.proj_type == 'std':
-            if QUANTIZE:
-                self.ortho_matrix = self._dequantize(self.ortho_matrix, QUANTIZE, self.quantization_state, low_rank_grad.dtype) # dequantize
+            if QUANTIZE: self.ortho_matrix = self._dequantize(self.ortho_matrix, QUANTIZE, self.quantization_state, low_rank_grad.dtype) # dequantize
             if low_rank_grad.shape[0] >= low_rank_grad.shape[1]:
                 full_rank_grad = torch.matmul(low_rank_grad, self.ortho_matrix)
             else:
                 full_rank_grad = torch.matmul(self.ortho_matrix, low_rank_grad)
-            if QUANTIZE:
-                self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ) # quantize back for storage
+            if QUANTIZE: self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ) # quantize back for storage
         elif self.proj_type == 'reverse_std':
+            if QUANTIZE: self.ortho_matrix = self._dequantize(self.ortho_matrix, QUANTIZE, self.quantization_state, low_rank_grad.dtype)
             if low_rank_grad.shape[0] <= low_rank_grad.shape[1]: # note this is different from std
                 full_rank_grad = torch.matmul(self.ortho_matrix, low_rank_grad)
             else:
                 full_rank_grad = torch.matmul(low_rank_grad, self.ortho_matrix)
+            if QUANTIZE: self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ)
         elif self.proj_type == 'right':
+            if QUANTIZE: self.ortho_matrix = self._dequantize(self.ortho_matrix, QUANTIZE, self.quantization_state, full_rank_grad.dtype)
             full_rank_grad = torch.matmul(low_rank_grad, self.ortho_matrix)
+            if QUANTIZE: self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ)
         elif self.proj_type == 'left':
+            if QUANTIZE: self.ortho_matrix = self._dequantize(self.ortho_matrix, QUANTIZE, self.quantization_state, full_rank_grad.dtype)
             full_rank_grad = torch.matmul(self.ortho_matrix, low_rank_grad)
+            if QUANTIZE: self.ortho_matrix, self.quantization_state = self._quantize(self.ortho_matrix, QUANTIZE, BLOCKSZ)
         elif self.proj_type == 'full':
             full_rank_grad = torch.matmul(self.ortho_matrix[0], low_rank_grad) @ self.ortho_matrix[1]
         
